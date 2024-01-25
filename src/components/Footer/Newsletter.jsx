93b92d516b59a58e2bd59266ab6f135a596ce784
Induwara <induwara2k@gmail.com>
2024-01-25 22:52:49 +0530

    book table and rooms section added

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,18 +1,22 @@
 import SubHeading from "../SubHeading/SubHeading";
 import "./Newsletter.css";
+import bookingRoom from '../../assets/bookingRoom.jpg';
+import bookingTable from '../../assets/bookingTable.jpg';
 
 const Newsletter = () => (
   <div className="app__newsletter">
     <div className="app__newsletter-heading">
-      <SubHeading title="Newsletter" />
-      <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
-      <p className="p__opensans">And never miss latest Updates!</p>
+      <SubHeading title="Booking" />
     </div>
-    <div className="app__newsletter-input flex__center">
-      <input type="email" placeholder="Enter your email address" />
-      <button type="button" className="custom__button">
-        Subscribe
-      </button>
+    <div className="app__newsletter-images">
+      <div className="image-container">
+        <img src={bookingRoom} alt="Booking Room" />
+        <div className="image-text">Rooms</div>
+      </div>
+      <div className="image-container">
+        <img src={bookingTable} alt="Booking Table" />
+        <div className="image-text">Tables</div>
+      </div>
     </div>
   </div>
 );
