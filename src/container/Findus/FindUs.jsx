c2e2dde0e6f3526ce4f068c7b279fe51a7257b5e
Induwara <induwara2k@gmail.com>
2024-01-25 23:27:58 +0530

    added map

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -10,7 +10,7 @@ const FindUs = () => (
       </h1>
       <div className="app__wrapper-content">
         <p className="p__opensans">
-        12 Hospital Street, 80000 Galle, Sri Lanka
+          12 Hospital Street, 80000 Galle, Sri Lanka
         </p>
         <p
           className="p__cormorant"
@@ -28,6 +28,15 @@ const FindUs = () => (
       >
         Visit Us
       </button>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31740.42371815527!2d80.19110635578782!3d6.055891161509118!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae1731fbc5b4d81%3A0x451b099022910ad4!2sLe%20Bella1912!5e0!3m2!1sen!2slk!4v1706203951001!5m2!1sen!2slk"
+        width="100%"
+        height="100%"
+        style={{ border: 0, marginTop: '2rem' }}
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Google Map"
+      />
     </div>
 
     <div className="app__wrapper_img">
