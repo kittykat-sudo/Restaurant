04230b0dec871e322d51399d53abe049ca778dee
kittykat-sudo <2022cs046@stu.ucsc.cmb.ac.lk>
2024-05-08 20:29:49 +0530

    otherpages.css

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,9 @@ import {
 import "./App.css";
 import OtherPage from "./container/HistoryDetails/OtherPage.jsx";
 
+// Import the RestaurantSchema component
+// import RestaurantSchema from './RestaurantSchema';
+
 const App = () => {
   const location = useLocation();
   const showNavbar = location.pathname !== '/special-menu';
@@ -28,6 +31,8 @@ const App = () => {
   return (
     <div>
       {showNavbar && <Navbar />}
+      {/* Include the RestaurantSchema component here */}
+      {/* <RestaurantSchema /> */}
       <Routes>
         <Route path="/" element={
           <>
