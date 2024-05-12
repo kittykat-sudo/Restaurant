864db93b25870b835d4a207583e97481aed36ff5
kittykat-sudo <2022cs046@stu.ucsc.cmb.ac.lk>
2024-05-12 22:42:52 +0530

    Adding chef image

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -8,27 +8,25 @@ const Chef = () => (
       <img src={images.chef} alt="chef_image" />
     </div>
     <div className="app__wrapper_info">
-      <SubHeading title="Chef's word" />
+      <SubHeading title="Owner's word" />
       <h1 className="headtext__cormorant">What we believe in</h1>
 
       <div className="app__chef-content">
         <div className="app__chef-content_quote">
           <img src={images.quote} alt="quote_image" />
           <p className="p__opensans">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .
+          At Le Bella 1912, we believe in the art of hospitality, where every guest is welcomed with warmth and treated with care.
           </p>
         </div>
         <p className="p__opensans">
           {" "}
-          auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit.
-          Nulla scelerisque scelerisque congue ac consequat, aliquam molestie
-          lectus eu. Congue iaculis integer curabitur semper sit nunc.{" "}
+          We believe in the joy of sharing good food with loved ones, creating moments that linger in memory. Our belief in quality, creativity, and community drives us to create exceptional dining experiences for all.{" "}
         </p>
       </div>
 
       <div className="app__chef-sign">
-        <p>Kevin Luo</p>
-        <p className="p__opensans">Chef & Founder</p>
+        <p>Lasantha</p>
+        <p className="p__opensans">Owner & Founder</p>
         <img src={images.sign} alt="sign_image" />
       </div>
     </div>
